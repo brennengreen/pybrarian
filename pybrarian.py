8f4fbee65a5bbfd5e7a38a8bc2e0326c5ff2b486
Brennen Green <32919777+brennengreen@users.noreply.github.com>
2018-06-25 15:34:16 -0400

    Update pybrarian.py

--- pybrarian.py
+++ pybrarian.py
@@ -1,26 +1,32 @@
-try:
-    import wikipedia as wiki
-except ImportError:
-    print("Error importing 'wikipedia', no module exists")
+import wikipedia as wiki
 
 
 class Book:
     def __init__(self, title, pages):
         self.book_title = title
-        try:
-            self.wiki_page = wiki.WikipediaPage(self.book_title)
-        except:
-            print("Multiple titles for that book are available, refining search..")
-            search = wiki.search(title,1)
-            refined_title = search[0]
-            self.wiki_page = wiki.WikipediaPage(refined_title)
-        try:
-            self.page_count = pages
-        except:
-            print("Page count not supplied, searching...")
-        self.book_summary = wiki.summary(self.book_title)
+        self.page_count = pages
+        
+        
+    def get_refined_title(self):
+        search = wiki.search(self.book_title,1)
+        refined_title = str(search[0])
+        if refined_title.find("book") > -1 or refined_title.find("novel") > -1:
+            self.book_title = refined_title
+            print(refined_title)
+            return refined_title
+        else:
+            search = wiki.search(self.book_title, 10)
+            for i in range(len(search)):
+                if search[i].find("book") > -1 or search[i].find("novel") > -1:
+                    refined_title = str(search[i])
+                    self.book_title = refined_title
+                    print(refined_title)
+                    return refined_title
+
+
 
     def get_book_pages(self):
+        self.wiki_page = wiki.WikipediaPage(self.get_refined_title())
         page_html = self.wiki_page.html()
         page_count = page_html.count("pages=")
         if page_count >= 1:
@@ -38,12 +44,15 @@ class Book:
                     count = page_html[next_index]
                 else:
                     count = count + page_html[next_index]
+            return count
 
     def get_book_summary(self):
+        self.book_summary = wiki.summary(self.get_refined_title())
         return self.book_summary
 
     def get_book_info(self):
         if self.page_count == None:
             self.get_book_pages()
         msg = "The title of this book is {0}, and there are {1} pages to read!"
+        
         return (msg.format(self.book_title, self.page_count))
